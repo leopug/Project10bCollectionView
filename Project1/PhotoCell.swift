39852b556ef190805a205796f1d61d2200366c52
leonardo maia pugliese <leo.pug@hotmail.com>
2020-01-19 20:42:43 -0300

    First commit

---
+++ Project1/PhotoCell.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoCell.swift
+//  Project1
+//
+//  Created by Ana Caroline de Souza on 19/01/20.
+//  Copyright © 2020 Ana e Leo Corp. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var photoName: UILabel!
+    
+    
+}
--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UICollectionViewController {
 
     var pictures = [String]()
     var picturesLabels = [String]()
@@ -18,12 +18,30 @@ class ViewController: UITableViewController {
 
         title = "Storm Viwer Da Massa"
         navigationController?.navigationBar.prefersLargeTitles = true
-        
-        performSelector(inBackground: #selector(loadImagesListFromBundle), with: nil)
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(shareTapped))
+        
+        loadImagesListFromBundle()
+        collectionView.reloadData()
+        
     }
     
-    @objc func loadImagesListFromBundle() {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as? PhotoCell else {
+            fatalError("Unable to dequeue PhotoCell.")
+        }
+        
+        cell.imageView.image = UIImage(named: pictures[indexPath.item])
+        cell.photoName.text = picturesLabels[indexPath.item]
+        
+        return cell
+    }
+    
+    
+    func loadImagesListFromBundle() {
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
@@ -31,35 +49,17 @@ class ViewController: UITableViewController {
         
         let itemsFilteredSorted = items.filter({$0.hasPrefix("nssl")}).sorted()
         for (index, item) in itemsFilteredSorted.enumerated() {
-            picturesLabels.append("Picture \(index+1) of \(itemsFilteredSorted.count)")
+            picturesLabels.append("Picture \(index+1)")
             pictures.append(item)
         }
         
-        DispatchQueue.main.async {
-          [weak self] in
-            self?.tableView.reloadData()
-        }
-        
-    }
-
-    func reloadTableAfterAsync(){
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return pictures.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-        cell.textLabel?.text = picturesLabels[indexPath.row]
-        return cell       
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
-            vc.selectedImage = pictures[indexPath.row]
-            navigationController?.pushViewController(vc, animated: true)
-        }
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+                if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+                    vc.selectedImage = pictures[indexPath.row]
+                    navigationController?.pushViewController(vc, animated: true)
+                }
     }
 
     @objc func shareTapped(){
